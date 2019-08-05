7661389e58cbf454b8513d6aefc4913b0024c1e7
Ansuman Ghosh <ansumanghosh@infrrd.ai>
2019-08-05 23:06:53 +0530

    Changed code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div style={{ height: '100vh', width: '100vw' }}>
+                <h1 className="display-1">1</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
