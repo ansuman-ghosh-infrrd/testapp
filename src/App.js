782aa848722167a727e150f541a5a3b52b7d5a1a
Ansuman Ghosh <ansumanghosh@infrrd.ai>
2019-08-10 19:02:37 +0530

    change 15

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <div
                 style={{ height: '100vh', width: '100vw' }}
                 className="bg-primary">
-                <h1 className="display-1">14</h1>
+                <h1 className="display-1">15</h1>
             </div>
         </div>
     );
